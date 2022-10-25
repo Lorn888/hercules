ce4621a05515eb9082a9459dca3d2e1cf69ba3b1
Patryk <patrykokpara@hotmail.com>
2022-10-25 23:13:41 +0100

    Succesfully tested connection to mongodb

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const workout = require('./models/workout')
+
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
+
+require('dotenv').config();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully http://192.168.0.229:3201/");
+})
+
+
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.listen(3200, () => {
+app.listen(3201, () => {
     console.log('Port is working :)')
-})
+}) 
