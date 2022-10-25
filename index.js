07d50028f8a3e3e7791eedd1f57f52203e9ffbc3
Patryk <patrykokpara@hotmail.com>
2022-10-25 12:47:11 +0100

    Launched the port

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.listen(3200, () => {
+    console.log('Port is working :)')
+})
