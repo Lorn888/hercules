55d0b822e3928ce7098586f3d54b4c8ca6a4ac5a
Patryk <patrykokpara@hotmail.com>
2023-03-30 22:02:32 +0100

    Refactoring

--- feed.js
+++ feed.js
@@ -1,41 +1,38 @@
-
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Workout = require("./models/workout")
+const Workout = require("./models/workout");
 
 const port = process.env.PORT || 5000;
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true }
-);
+mongoose.connect(uri, { useNewUrlParser: true });
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
 app.get("/workout", (req, res) => {
-    res.send("Workouts will be here")
-})
+  res.send("Workouts will be here");
+});
 
-
-const stuff = new Workout ({
-    username: "jada",
-    exercise: "Cheering",
-    duration: 60,
-    mood: "Amazing"
-})
-stuff.save().then(stuff => {
-    console.log(stuff)
-})
-.catch(e => {
+const stuff = new Workout({
+  username: "jada",
+  exercise: "Cheering",
+  duration: 60,
+  mood: "Amazing",
+});
+stuff
+  .save()
+  .then((stuff) => {
+    console.log(stuff);
+  })
+  .catch((e) => {
     console.log(e);
-})
+  });
 
 app.listen(5000, () => {
-    console.log("We are going on the port")
-})
+  console.log("We are going on the port");
+});
--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const Workout = require('./models/workout')
+const path = require("path");
+const Workout = require("./models/workout");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true }
-);
+mongoose.connect(uri, { useNewUrlParser: true });
 const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully http://192.168.0.229:3201/");
-})
+connection.once("open", () => {
+  console.log(
+    "MongoDB database connection established successfully http://192.168.0.229:3201/"
+  );
+});
 
-
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
-  const workouts = await Workout.find({})
-  console.log(workouts)
-  res.render("home", {workouts})
-})
+  const workouts = await Workout.find({});
+  console.log(workouts);
+  res.render("home", { workouts });
+});
 
 app.listen(3201, () => {
-    console.log('Port is working :)')
-}) 
+  console.log("Port is working :)");
+});
